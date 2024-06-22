690a7dbd1c6fd12ae541a8c269fe99f9aa1a967f
Globox1997 <mitschke11@web.de>
2024-06-22 10:57:02 +0200

    updated to mc 1.21

--- src/main/java/net/herdspanic/mixin/EscapeDangerGoalMixin.java
+++ src/main/java/net/herdspanic/mixin/EscapeDangerGoalMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.At;
 
 import net.herdspanic.HerdsPanicMain;
@@ -40,6 +41,13 @@ public abstract class EscapeDangerGoalMixin extends Goal {
             }
         }
 
+    }
+
+    @Inject(method = "isInDanger", at = @At("HEAD"), cancellable = true)
+    protected void isInDangerMixin(CallbackInfoReturnable<Boolean> info) {
+        if (mob.getAttacker() != null) {
+            info.setReturnValue(true);
+        }
     }
 
 }
